21e807aba624aaeb0f4653127be237b93793493b
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-08-19 16:53:04 +0200

    add another case

--- frontend/schemaUI/components/card/card.stories.js
+++ frontend/schemaUI/components/card/card.stories.js
@@ -3,21 +3,32 @@ import { storiesOf } from '@storybook/react';
 
 import { Card } from './card.component';
 
-const defaultProps = {};
-
 storiesOf('Card', module).add('Default', () => (
   <Card>
     <span>This is a <b>card</b> content</span>
   </Card>
 ));
 
-const extendedProps = {
-  ...defaultProps,
+const withStyleProps = {
+  style: {color: 'green'}
+};
+
+storiesOf('Card', module).add('with style', () => (
+  <Card {...withStyleProps} >
+    <p>This is a <em>green</em> card.</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a auctor ante.
+      Ut at lorem id elit elementum venenatis. Quisque vulputate sit amet odio eu dictum.
+    </p>
+  </Card>
+));
+
+const withHeaderProps = {
   headerComponent: <b>Header</b>
 };
 
 storiesOf('Card', module).add('with header', () => (
-  <Card {...extendedProps} >
+  <Card {...withHeaderProps} >
     <p>This is a <em>card</em> with <b>header</b> content.</p>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a auctor ante.
