784ad00cc485dc21f19db43af4bc73a74121771a
Hamza Akbar <ihamzakbar@Hamzas-Mac.local>
2019-08-21 16:22:01 +0500

    removing unused imports

--- backend/schemacms/projects/models.py
+++ backend/schemacms/projects/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.conf import settings
-from django.utils import timezone
 from django.template.defaultfilters import slugify
 from django_extensions.db.fields import AutoSlugField
 
