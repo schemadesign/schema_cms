1ab4d57fd9770ed2213ce6a32b0db717adf54eb5
Hamza Akbar <ihamzakbar@Hamzas-Mac.local>
2019-08-21 16:19:33 +0500

    fix for created and modified fields

--- backend/schemacms/projects/models.py
+++ backend/schemacms/projects/models.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from django.template.defaultfilters import slugify
 from django_extensions.db.fields import AutoSlugField
 
+
 # Create your models here.
 class Projects(models.Model):
     status_choices = (
@@ -13,16 +14,12 @@ class Projects(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     slug = AutoSlugField(max_length=255, unique=True, populate_from=('title',))
-    created = models.DateTimeField(editable=False)
-    modified = models.DateTimeField(editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=25, choices=status_choices)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
-        if not self.id:
-            self.created = timezone.now()
-            self.modified = timezone.now()
-        self.modified = timezone.now()
         self.slug = slugify(self.title)
         super(Projects, self).save(*args, **kwargs)
 
