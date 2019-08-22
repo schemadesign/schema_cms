762fafb5341a8d7ab0f3c95276f37f450ecf5b2e
Daniel Capeletti <dcapeletti@icloud.com>
2019-08-22 13:17:20 +0200

    Add userAuth redux test

--- frontend/schemaCMS/src/modules/userAuth/__tests__/userAuth.redux.spec.js
+++ frontend/schemaCMS/src/modules/userAuth/__tests__/userAuth.redux.spec.js
@@ -4,7 +4,10 @@ import Immutable from 'seamless-immutable';
 import { reducer as userAuthReducer, UserAuthActions, UserAuthTypes } from '../userAuth.redux';
 
 describe('UserAuth: redux', () => {
-  const state = Immutable({});
+  const defaultState = Immutable({
+    isAuthenticated: false,
+    jwtToken: null,
+  });
 
   describe('reducer', () => {
     it('should return initial state', () => {
@@ -13,6 +16,20 @@ describe('UserAuth: redux', () => {
 
     it('should return state on unknown action', () => {
       expect(userAuthReducer(state, { type: 'unknown-action' })).to.deep.equal(state);
+    });
+  });
+
+  describe('when USER_AUTH/GET_JWT_TOKEN_SUCCESS action is received', () => {
+    it('should set isAuthenticated to true', () => {
+      const token = '12345';
+      const resultState = userAuthReducer(defaultState, UserAuthActions.getJwtTokenSuccess(token));
+      expect(resultState.isAuthenticated).to.deep.equal(true);
+    });
+
+    it('should set jwtToken value', () => {
+      const token = '12345';
+      const resultState = userAuthReducer(defaultState, UserAuthActions.getJwtTokenSuccess(token));
+      expect(resultState.jwtToken).to.deep.equal(token);
     });
   });
 });
