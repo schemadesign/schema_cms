b50141a3a5ab4e1b582f2cdb00099b9a7d9a8f91
Hamza Akbar <hamza.akbar@schemadesign.com>
2019-08-22 16:44:21 +0500

    Adding Status Field in users model

--- backend/schemacms/users/serializers.py
+++ backend/schemacms/users/serializers.py
@@ -3,8 +3,7 @@ from .models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name',)
-        read_only_fields = ('username', )
+        read_only_fields = ('username', 'status')
