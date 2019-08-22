5618bee4247c462c87177eff741700eaaa1f826a
Krystian Hanek <khanek@apptension.com>
2019-08-22 10:01:38 +0200

    Restore cors and configs, fix test with deleting __init__.py in backend dir, update docs for projects, add project django app to INSTALLED_APPS

--- backend/schemacms/config/local.py
+++ backend/schemacms/config/local.py
@@ -6,9 +6,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 class Local(Common):
     DEBUG = True
 
-    INSTALLED_APPS = Common.INSTALLED_APPS + (
-    )
-
     # Mail
     EMAIL_HOST = 'mailcatcher'
     EMAIL_PORT = 1025
