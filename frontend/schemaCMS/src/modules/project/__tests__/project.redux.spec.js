6839366cabdd5c69ddaf16c02bfc3eea84082924
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-08-27 16:09:26 +0200

    add test

--- frontend/schemaCMS/src/modules/project/__tests__/project.redux.spec.js
+++ frontend/schemaCMS/src/modules/project/__tests__/project.redux.spec.js
@@ -1,21 +1,42 @@
 import { expect } from 'chai';
 import Immutable from 'seamless-immutable';
 
-import { reducer as projectReducer } from '../project.redux';
+import { reducer as projectReducer, ProjectActions } from '../project.redux';
 
 describe('Project: redux', () => {
-  const state = Immutable({
+  const defaultState = Immutable({
     projects: [],
-    isMenuOpen: false,
   });
 
   describe('reducer', () => {
     it('should return initial state', () => {
-      expect(projectReducer(undefined, {})).to.deep.equal(state);
+      expect(projectReducer(undefined, {})).to.deep.equal(defaultState);
     });
 
     it('should return state on unknown action', () => {
-      expect(projectReducer(state, { type: 'unknown-action' })).to.deep.equal(state);
+      expect(projectReducer(defaultState, { type: 'unknown-action' })).to.deep.equal(defaultState);
+    });
+  });
+
+  describe('when PROJECTS/FETCH_LIST_SUCCESS action is received', () => {
+    it('should set projects value', () => {
+      const projects = [
+        {
+          title: 'Project Name',
+          description: 'Description',
+          slug: 'schemacms/api/project_title',
+          created: '2019-08-26T11:05:12+0000',
+          status: 'Status',
+          owner: {
+            id: '1',
+            firstName: 'Firstname',
+            lastName: 'Lastname',
+          },
+        },
+      ];
+
+      const resultState = projectReducer(defaultState, ProjectActions.fetchListSuccess(projects));
+      expect(resultState.projects).to.deep.equal(projects);
     });
   });
 });
