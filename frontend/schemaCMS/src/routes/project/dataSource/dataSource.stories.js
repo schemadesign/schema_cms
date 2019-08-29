2e110cbad31256a3c9623e95d62becd3cd563c24
Rafał Gruszecki <rgruszecki@apptension.com>
2019-08-29 11:41:18 +0200

    Remove unused files

--- frontend/schemaCMS/src/routes/project/dataSource/dataSource.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import { DataSource } from './dataSource.component';
-
-const defaultProps = {};
-
-storiesOf('DataSource', module).add('Default', () => <DataSource {...defaultProps} />);
