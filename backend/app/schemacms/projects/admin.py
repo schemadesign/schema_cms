6ac62b322c7e79169f507fbc0606efdcfadb59ad
szdrasiak <sjedrasiak@apptension.com>
2019-09-02 11:35:56 +0200

    adding datasource via django admin fix

--- backend/app/schemacms/projects/admin.py
+++ backend/app/schemacms/projects/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.db import transaction
+
 from .models import DataSource, Project
 
 
-admin.site.register(DataSource)
 admin.site.register(Project)
+
+
+@admin.register(DataSource)
+class DataSource(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        file = obj.file
+        obj.file = None
+        with transaction.atomic():
+            super().save_model(request, obj, form, change)
+            obj.file.save(file.name, file)
