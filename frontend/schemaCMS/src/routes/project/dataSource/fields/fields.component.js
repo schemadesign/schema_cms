3c7b8a340142a58ab1b41b2ec363465c79461990
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-09-06 15:19:24 +0200

    add data grid to view

--- frontend/schemaCMS/src/routes/project/dataSource/fields/fields.component.js
+++ frontend/schemaCMS/src/routes/project/dataSource/fields/fields.component.js
@@ -1,10 +1,15 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import { DataGrid } from 'schemaUI';
+import { isEmpty } from 'ramda';
 
 import { TopHeader } from '../../../../shared/components/topHeader';
+import { Loader } from '../../../../shared/components/loader';
 import messages from './fields.messages';
 import { Container } from './fields.styles';
+
+const INITIAL_STEP = 0;
 
 export class Fields extends PureComponent {
   static propTypes = {
@@ -21,10 +26,25 @@ export class Fields extends PureComponent {
     }).isRequired,
   };
 
+  state = {
+    step: INITIAL_STEP,
+  };
+
   componentDidMount() {
     const { projectId, dataSourceId } = this.props.match.params;
 
     this.props.fetchFields({ projectId, dataSourceId });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.step);
+    const isLoading = this.isLoading();
+
+    if (!isLoading && !this.state.maxSteps) {
+      this.setState({
+        maxSteps: Object.keys(this.props.fields).length,
+      });
+    }
   }
 
   componentWillUnmount() {
@@ -37,14 +57,28 @@ export class Fields extends PureComponent {
     secondaryMenuItems: [],
   });
 
+  isLoading = () => isEmpty(this.props.fields);
+
+  renderTable() {
+    const rows = this.props.previewTable.map((row, rowIndex) => ({ ...row, rowIndex }));
+    const columnsIds = ['rowIndex', ...Object.keys(this.props.fields)];
+    const columns = columnsIds.map(name => ({ name, displayName: name }));
+    const data = { columns, rows };
+
+    return <DataGrid data={data} />;
+  }
+
   render() {
     const topHeaderConfig = this.getHeaderAndMenuConfig('Knoll Archive');
 
     console.log(this.props);
+    const content = isEmpty(this.props.fields) ? <Loader /> : this.renderTable();
+
     return (
       <Container>
         <Helmet title={this.props.intl.formatMessage(messages.pageTitle)} />
         <TopHeader {...topHeaderConfig} />
+        {content}
       </Container>
     );
   }
