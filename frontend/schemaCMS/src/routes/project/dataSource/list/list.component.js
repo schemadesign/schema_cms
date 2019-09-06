414b79755ac21d6e8467343f57445fbd3477c351
Rafał Gruszecki <rgruszecki@apptension.com>
2019-09-06 16:11:25 +0200

    Pre style a dataSource list component

--- frontend/schemaCMS/src/routes/project/dataSource/list/list.component.js
+++ frontend/schemaCMS/src/routes/project/dataSource/list/list.component.js
@@ -1,21 +1,46 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { Button, Card, Icons, Typography } from 'schemaUI';
 
-import { Button, Icons } from 'schemaUI';
 import { TopHeader } from '../../../../shared/components/topHeader';
-import { addDataSourceStyles, Container } from './list.styles';
+import {
+  addDataSourceStyles,
+  Container,
+  DataSourceItem,
+  DataSourceList,
+  titleStyles,
+  MetaDataWrapper,
+  MetaData,
+  MetaDataName,
+  MetaDataValue,
+} from './list.styles';
 import messages from './list.messages';
+import extendedDayjs from '../../../../shared/utils/extendedDayjs';
+import { HeaderItem, HeaderList } from '../../list/list.styles';
+
+const { H1 } = Typography;
+const { PlusIcon, CsvIcon } = Icons;
 
 export class List extends PureComponent {
   static propTypes = {
     createDataSource: PropTypes.func.isRequired,
+    fetchDataSources: PropTypes.func.isRequired,
+    dataSources: PropTypes.array.isRequired,
     intl: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired,
     match: PropTypes.shape({
       params: PropTypes.shape({
         projectId: PropTypes.string.isRequired,
       }).isRequired,
     }).isRequired,
   };
+
+  componentDidMount() {
+    const projectId = this.props.match.params.projectId;
+
+    this.props.fetchDataSources({ projectId });
+  }
 
   getHeaderAndMenuConfig = () => ({
     headerTitle: this.props.intl.formatMessage(messages.title),
@@ -34,14 +59,68 @@ export class List extends PureComponent {
     this.props.createDataSource({ projectId });
   };
 
+  handleShowDataSource = id =>
+    this.props.history.push(`/project/view/${this.props.match.params.projectId}/dataSource/view/${id}`);
+
+  renderHeader = (list = []) => (
+    <HeaderList>
+      {list.map((item, index) => (
+        <HeaderItem key={index}>{item}</HeaderItem>
+      ))}
+    </HeaderList>
+  );
+
+  renderMetaData = ({ items, fields, filters, views }) => {
+    const list = [
+      { name: 'Source', value: <CsvIcon customStyles={{ width: 40, height: 40 }} /> },
+      { name: 'Items', value: items || '000' },
+      { name: 'Fields', value: fields || '00' },
+      { name: 'Filters', value: filters || '00' },
+      { name: 'Views', value: views || '00' },
+    ];
+
+    const elements = list.map(({ name, value }, index) => (
+      <MetaData key={index}>
+        <MetaDataName>{name}</MetaDataName>
+        <MetaDataValue>{value}</MetaDataValue>
+      </MetaData>
+    ));
+
+    return <MetaDataWrapper>{elements}</MetaDataWrapper>;
+  };
+
+  renderItem({ name, createdAt = '2019-08-29T07:23:50+0000', createdBy = 'Rafcio', id, metaData }, index) {
+    const whenCreated = extendedDayjs(createdAt).fromNow();
+    const header = this.renderHeader([whenCreated, createdBy]);
+
+    return (
+      <DataSourceItem key={index}>
+        <Card headerComponent={header}>
+          <H1 customStyles={titleStyles} onClick={() => this.handleShowDataSource(id)}>
+            {name}
+          </H1>
+          {this.renderMetaData(metaData || {})}
+        </Card>
+      </DataSourceItem>
+    );
+  }
+
+  renderList = dataSources => (
+    <DataSourceList>{dataSources.map((item, index) => this.renderItem(item, index))}</DataSourceList>
+  );
+
   render() {
+    const { dataSources = [] } = this.props;
+
     const topHeaderConfig = this.getHeaderAndMenuConfig();
 
     return (
       <Container>
+        <Helmet title={this.props.intl.formatMessage(messages.title)} />
         <TopHeader {...topHeaderConfig} />
+        {this.renderList(dataSources)}
         <Button customStyles={addDataSourceStyles} onClick={this.handleCreateDataSource}>
-          <Icons.PlusIcon />
+          <PlusIcon />
         </Button>
       </Container>
     );
--- frontend/schemaCMS/src/routes/project/dataSource/list/list.styles.js
+++ frontend/schemaCMS/src/routes/project/dataSource/list/list.styles.js
@@ -1,7 +1,9 @@
 import styled from 'styled-components';
 import { Theme } from 'schemaUI';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  margin: 0 20px 60px 20px;
+`;
 
 export const addDataSourceStyles = {
   position: 'fixed',
@@ -10,3 +12,39 @@ export const addDataSourceStyles = {
   backgroundColor: Theme.primary.label,
   height: '60px',
 };
+
+export const DataSourceItem = styled.li`
+  margin-top: 10px;
+
+  &:first-child {
+    margin-top: 0;
+  }
+`;
+
+export const Description = styled.div`
+  margin: 7px 0 14px;
+  cursor: pointer;
+`;
+
+export const DataSourceList = styled.div`
+  list-style: none;
+  padding: 0;
+`;
+
+export const titleStyles = {
+  cursor: 'pointer',
+};
+
+export const MetaDataWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const MetaData = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const MetaDataName = styled.span``;
+export const MetaDataValue = styled.span``;
