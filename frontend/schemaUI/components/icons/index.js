5b663f87930fe9ecf736eeaa4f89b51b8f9df9ed
Rafał Gruszecki <rgruszecki@apptension.com>
2019-09-09 11:16:18 +0200

    Add intersection icon to UI

--- frontend/schemaUI/components/icons/index.js
+++ frontend/schemaUI/components/icons/index.js
@@ -5,8 +5,9 @@ import { PlusIcon } from './plusIcon';
 import { EditIcon } from './editIcon';
 import { CsvIcon } from './csvIcon';
 import { UploadIcon } from './uploadIcon';
+import { IntersectIcon } from './intersectIcon';
 
-const Icons = { ArrowLeftIcon, CloseIcon, MenuIcon, PlusIcon, EditIcon, CsvIcon, UploadIcon };
+const Icons = { ArrowLeftIcon, CloseIcon, MenuIcon, PlusIcon, EditIcon, CsvIcon, UploadIcon, IntersectIcon };
 
-export { ArrowLeftIcon, CloseIcon, MenuIcon, PlusIcon, EditIcon, CsvIcon, UploadIcon };
+export { ArrowLeftIcon, CloseIcon, MenuIcon, PlusIcon, EditIcon, CsvIcon, UploadIcon, IntersectIcon };
 export default Icons;
---
+++ frontend/schemaUI/components/icons/intersectIcon/index.js
@@ -0,0 +1,1 @@
+export { IntersectIcon } from './intersectIcon.component';
