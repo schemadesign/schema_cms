39d2af3198c21d5a317febaa71aeb1bd1f013f8e
Daniel Capeletti <dcapeletti@icloud.com>
2019-09-09 10:26:50 +0200

    Add tests

--- frontend/schemaCMS/src/routes/logout/__tests__/logout.component.spec.js
+++ frontend/schemaCMS/src/routes/logout/__tests__/logout.component.spec.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+import { expect } from 'chai';
 
 import { Logout } from '../logout.component';
 
 describe('Logout: Component', () => {
-  const defaultProps = {};
+  const defaultProps = {
+    logout: Function.prototype,
+  };
 
   const component = props => <Logout {...defaultProps} {...props} />;
 
@@ -13,5 +17,12 @@ describe('Logout: Component', () => {
   it('should render correctly', () => {
     const wrapper = render();
     global.expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call logout function on componentDidMount', () => {
+    const logout = spy();
+    render({ logout  });
+
+    expect(logout).to.have.been.calledOnce;
   });
 });
--- frontend/schemaCMS/src/routes/resetPassword/__tests__/resetPassword.component.spec.js
+++ frontend/schemaCMS/src/routes/resetPassword/__tests__/resetPassword.component.spec.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import { spy } from 'sinon';
+import { expect } from 'chai';
 
 import { ResetPassword } from '../resetPassword.component';
 
 describe('ResetPassword: Component', () => {
-  const defaultProps = {};
+  const defaultProps = {
+    resetPassword: Function.prototype,
+  };
 
   const component = props => <ResetPassword {...defaultProps} {...props} />;
 
@@ -13,5 +17,12 @@ describe('ResetPassword: Component', () => {
   it('should render correctly', () => {
     const wrapper = render();
     global.expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should call resetPassword function on componentDidMount', () => {
+    const resetPassword = spy();
+    render({ resetPassword });
+
+    expect(resetPassword).to.have.been.calledOnce;
   });
 });
