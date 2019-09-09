f35c882f0708c59ea926f858d2f00f54dd9171e8
Krystian Hanek <khanek@apptension.com>
2019-09-09 11:13:08 +0200

    [CMS-149] Catch error from auth0 when inviting user from admin panel, catch error from auth0 on creating change password ticket when user login by social account (#66)

--- backend/app/schemacms/users/tests/test_backend_management.py
+++ backend/app/schemacms/users/tests/test_backend_management.py
@@ -1,7 +1,8 @@
+import auth0.v3
 import pytest
 
 from schemacms.users import constants
-from schemacms.users.backend_management import auth0
+from schemacms.users.backend_management import auth0 as auth0_
 
 pytestmark = [pytest.mark.django_db]
 
@@ -9,7 +10,7 @@ pytestmark = [pytest.mark.django_db]
 class TestAuth0UserManagement:
     def test_create_user_with_name(self, mocker, user_factory, auth0_management):
         user = user_factory(first_name="Firstname", last_name="Lastname")
-        mgmt = auth0.Auth0UserManagement()
+        mgmt = auth0_.Auth0UserManagement()
 
         mgmt.create_user(user)
 
@@ -28,7 +29,7 @@ class TestAuth0UserManagement:
 
     def test_create_user_without_name(self, mocker, user_factory, auth0_management):
         user = user_factory(first_name="", last_name="")
-        mgmt = auth0.Auth0UserManagement()
+        mgmt = auth0_.Auth0UserManagement()
 
         mgmt.create_user(user)
 
@@ -44,7 +45,19 @@ class TestAuth0UserManagement:
         )
 
     def test_get_user_source(self):
-        assert auth0.Auth0UserManagement().get_user_source() == constants.UserSource.AUTH0
+        assert auth0_.Auth0UserManagement().get_user_source() == constants.UserSource.AUTH0
+
+    def test_password_change_url(self, user, auth0_management):
+        mgmt = auth0_.Auth0UserManagement()
+        mgmt.proxy.tickets.create_pswd_change.side_effect = auth0.v3.Auth0Error(
+            status_code=400,
+            error_code="operation_not_supported",
+            message="The user's main connection does not support this operation",
+        )
+
+        ret = mgmt.password_change_url(user)
+
+        assert ret == mgmt.get_login_url()
 
     @pytest.fixture(autouse=True)
     def setup(self, settings):
