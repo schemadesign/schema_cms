4c967f48d1c81c0ac6c3ec85a6db2f0277b9ed9e
Krystian Hanek <khanek@apptension.com>
2019-09-11 15:00:32 +0200

    [CMS-172] Add middleware to catch exceptions from social django

--- backend/app/schemacms/config/common.py
+++ backend/app/schemacms/config/common.py
@@ -65,6 +65,7 @@ class Common(Configuration):
         "django.contrib.auth.middleware.AuthenticationMiddleware",
         "django.contrib.messages.middleware.MessageMiddleware",
         "django.middleware.clickjacking.XFrameOptionsMiddleware",
+        "schemacms.config.middlewares.SocialAuthExceptionMiddleware",
     )
 
     ALLOWED_HOSTS = ["*"]
