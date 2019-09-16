a5cbee6e5e6ca2abffc85923700e8b8539df1848
mkleszcz <mkleszcz@users.noreply.github.com>
2019-09-16 15:57:34 +0200

    Set up HTTPS (#101)

--- app.py
+++ app.py
@@ -2,13 +2,14 @@
 
 from aws_cdk import core
 
-from schema_cms_stack.schema_cms_stack import API, Workers, BaseResources, PublicAPI, CIPipeline,\
+from schema_cms_stack.schema_cms_stack import API, Workers, BaseResources, PublicAPI, CIPipeline, CertsStack,\
     INSTALLATION_MODE_FULL, INSTALLATION_MODE_CONTEXT_KEY
 
 
 class App(core.App):
     def __init__(self,  **kwargs) -> None:
         super().__init__(**kwargs)
+        self.certs = CertsStack(self, 'certs')
         self.base = BaseResources(self, 'base')
         self.workers = Workers(self, 'workers')
         self.api = API(self, 'api')
