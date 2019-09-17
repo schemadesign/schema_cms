d49b738dd7e8cb61d4c50c0648ad149dec9f48fe
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2019-09-17 16:46:39 +0200

    Replace password with random

--- backend/app/schemacms/users/management/commands/initialuser.py
+++ backend/app/schemacms/users/management/commands/initialuser.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+from django.utils import crypto
 
 from schemacms.users import models
 
@@ -12,9 +13,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         existing_user = models.User.objects.filter(username=NEW_USER_USERNAME).exists()
         if not existing_user:
+            password = crypto.get_random_string(10)
             models.User.objects.create_superuser(
-                NEW_USER_USERNAME, f'{NEW_USER_USERNAME}@apptension.com', 'm@S*8Xg$KuwCX%wV'
+                NEW_USER_USERNAME, f'{NEW_USER_USERNAME}@apptension.com', password
             )
-            self.stdout.write(self.style.SUCCESS(f'{NEW_USER_USERNAME} user created'))
+            self.stdout.write(self.style.SUCCESS(f'{NEW_USER_USERNAME} user created: {password} ℹ️'))
         else:
             self.stdout.write(self.style.WARNING(f'{NEW_USER_USERNAME} user seems to already exist 👀'))
