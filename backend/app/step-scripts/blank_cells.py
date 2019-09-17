4fc39c503af3479e97713a5b87911f4ab4784d57
Aleksander Ambrozkiewicz <aambrozkiewicz@apptension.com>
2019-09-17 12:42:08 +0200

    Trailing whitespace

--- backend/app/step-scripts/blank_cells.py
+++ backend/app/step-scripts/blank_cells.py
@@ -41,10 +41,10 @@ df = df["A"].fillna(method='bfill', inplace=True)
 
 """
 3. Drop rows
-It is not possible to just drop one blank cell. we would need to drop the whole row or column to 
+It is not possible to just drop one blank cell. we would need to drop the whole row or column to
 clean our dataset so there can be two cases of blank cells in rows and columns.
-    1. All cells are blank (empty row or column)
-    2. One or more cells are blank (partially empty row or column)
+1. All cells are blank (empty row or column)
+2. One or more cells are blank (partially empty row or column)
 """
 # Empty Rows
 df = df.dropna(how='all', axis=0)
