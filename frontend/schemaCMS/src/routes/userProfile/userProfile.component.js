289d03604a675c89a37c713e9e2892335e3eeeae
Daniel Capeletti <dcapeletti@icloud.com>
2019-09-20 12:53:36 +0200

    test

--- frontend/schemaCMS/src/routes/userProfile/userProfile.component.js
+++ frontend/schemaCMS/src/routes/userProfile/userProfile.component.js
@@ -61,6 +61,7 @@ export class UserProfile extends PureComponent {
         </Form>
         <LinksWrapper>
           <Link to="/reset-password">{intl.formatMessage(messages.resetPassword)}</Link>
+
           <Link to="/logout">{intl.formatMessage(messages.logout)}</Link>
         </LinksWrapper>
       </Container>
