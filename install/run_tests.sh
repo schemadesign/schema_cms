6ffd3876fa5e124349c9a4515553eb468e9def34
Daniel Capeletti <dcapeletti@icloud.com>
2019-09-20 12:03:45 +0200

    Use CI=true to not fail hooks

--- install/run_tests.sh
+++ install/run_tests.sh
@@ -5,8 +5,8 @@
 SCHEMA_UI_PATH="./frontend/schemaUI"
 SCHEMA_CMS_PATH="./frontend/schemaCMS"
 
-yarn --cwd $SCHEMA_UI_PATH test --no-watch
-yarn --cwd $SCHEMA_UI_PATH build
-yarn --cwd $SCHEMA_CMS_PATH test --no-watch
+CI=true yarn --cwd $SCHEMA_UI_PATH test
+CI=true yarn --cwd $SCHEMA_UI_PATH build
+CI=true yarn --cwd $SCHEMA_CMS_PATH test
 docker-compose exec backend pytest
 
