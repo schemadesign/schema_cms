bf321c7971c44808de29f5264534829d75bb8867
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-09-20 08:56:54 +0200

    fix getting date on Safari

--- frontend/schemaCMS/src/shared/utils/extendedDayjs.js
+++ frontend/schemaCMS/src/shared/utils/extendedDayjs.js
@@ -1,6 +1,9 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
 
-dayjs.extend(relativeTime);
+dayjs.extend(relativeTime).extend(customParseFormat);
 
 export default dayjs;
+
+export const BASE_DATE_FORMAT = 'YYYY-MM-DDTHH:mm:ss.000ZZ';
