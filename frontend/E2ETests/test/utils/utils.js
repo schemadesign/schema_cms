4d34a583b7cfab989ce37a5b5b3935da4c200b85
Arkadiusz Buchholz <abuchholz@apptension.com>
2019-09-23 13:12:43 +0200

    Create utility functions for waiting for elements

---
+++ frontend/E2ETests/test/utils/utils.js
@@ -0,0 +1,9 @@
+import { TIMEOUT } from './../constants/config.constants.js';
+
+export const waitForElement = (that, elemName, timeout = TIMEOUT) => {
+    browser.waitUntil(() => that[elemName].isDisplayed(),timeout, `${elemName} not load after 10 seconds`);
+}
+
+export const waitForText = (that, elemName, text, timeout = TIMEOUT) => {
+    browser.waitUntil(() => that[elemName].getText() === text, timeout, `Text of ${elemName} doesn't equal ${text}`);
+}
