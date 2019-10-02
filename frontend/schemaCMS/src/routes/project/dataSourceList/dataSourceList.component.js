8cbe6b8b7fb69a2301efba765380cf726e3d1a30
Rafał Gruszecki <rgruszecki@apptension.com>
2019-10-02 14:33:14 +0200

    Add sorting data source by created on frontend

--- frontend/schemaCMS/src/routes/project/dataSourceList/dataSourceList.component.js
+++ frontend/schemaCMS/src/routes/project/dataSourceList/dataSourceList.component.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { Card, Icons, Typography } from 'schemaUI';
-import { always, anyPass, cond, equals, ifElse } from 'ramda';
+import { always, anyPass, pipe, cond, equals, ifElse, sort, prop, descend } from 'ramda';
 import { FormattedMessage } from 'react-intl';
 import dayjs from 'dayjs';
 
@@ -206,6 +206,11 @@ export class DataSourceList extends PureComponent {
     );
   };
 
+  renderList = pipe(
+    sort(descend(prop('created'))),
+    data => data.map(this.renderItem)
+  );
+
   render() {
     const { dataSources = [] } = this.props;
     const topHeaderConfig = this.getHeaderAndMenuConfig();
@@ -214,7 +219,7 @@ export class DataSourceList extends PureComponent {
       <Container>
         <Helmet title={this.props.intl.formatMessage(messages.title)} />
         <TopHeader {...topHeaderConfig} />
-        <DataSourceListWrapper>{dataSources.map(this.renderItem)}</DataSourceListWrapper>
+        <DataSourceListWrapper>{this.renderList(dataSources)}</DataSourceListWrapper>
 
         <NavigationContainer>
           <BackArrowButton onClick={this.handleShowProject} />
