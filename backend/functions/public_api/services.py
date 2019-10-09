981cf3665250b999d5e2dd976932225c40c28cc2
Krystian Hanek <khanek@apptension.com>
2019-10-09 17:18:36 +0200

    Public-api lambda deployment

---
+++ backend/functions/public_api/services.py
@@ -0,0 +1,14 @@
+import boto3
+
+import settings
+
+
+s3 = boto3.client(
+    's3',
+    endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+)
+
+s3_resource = boto3.resource(
+    "s3",
+    endpoint_url=settings.AWS_S3_ENDPOINT_URL,
+)
