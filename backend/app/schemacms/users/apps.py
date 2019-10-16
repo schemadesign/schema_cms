fc73f892876c8f3fac62a8d48081a2cbd74bb309
Krystian Hanek <khanek@apptension.com>
2019-10-16 14:58:01 +0200

    Add sender to signal connect method

--- backend/app/schemacms/users/apps.py
+++ backend/app/schemacms/users/apps.py
@@ -9,10 +9,15 @@ class UsersConfig(AppConfig):
 
     def ready(self):
         from . import receivers  # noqa
+        from . import models  # noqa
 
         signals.user_deactivated.connect(
-            receivers.remove_from_projects, dispatch_uid="users.receivers.remove_from_projects"
+            receivers.remove_from_projects,
+            sender=models.User,
+            dispatch_uid="users.receivers.remove_from_projects",
         )
         db_signals.post_delete.connect(
-            receivers.remove_from_auth_backend, dispatch_uid="users.receivers.remove_from_auth_backend"
+            receivers.remove_from_auth_backend,
+            sender=models.User,
+            dispatch_uid="users.receivers.remove_from_auth_backend",
         )
