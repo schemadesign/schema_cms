c1602789f3e52b99ab4cae567329f4c0ffbc1f26
Krystian Hanek <khanek@apptension.com>
2019-10-18 16:43:33 +0200

    [CMS-326] Increase limit for file field [CMS-327] Improve error handling for job processing

---
+++ backend/functions/worker/errors.py
@@ -0,0 +1,9 @@
+class JobLoadingSourceFileError(Exception):
+    pass
+
+
+class JobSetExecutionError(Exception):
+    def __init__(self, msg, step, *args):
+        self.msg = msg
+        self.step = step
+        super().__init__(*args)
