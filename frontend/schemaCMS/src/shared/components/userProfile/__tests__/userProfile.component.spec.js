73e182d14e60c90a6c8542a970f9c7b50a6551e8
Rafał Gruszecki <rgruszecki@apptension.com>
2019-10-21 10:37:49 +0200

    Update user profile tests

--- frontend/schemaCMS/src/shared/components/userProfile/__tests__/userProfile.component.spec.js
+++ frontend/schemaCMS/src/shared/components/userProfile/__tests__/userProfile.component.spec.js
@@ -3,8 +3,10 @@ import { shallow } from 'enzyme';
 import { Formik } from 'formik';
 
 import { UserProfile } from '../userProfile.component';
+import { Form } from '../userProfile.styles';
 import { defaultProps } from '../userProfile.stories';
-import { BackButton, NextButton } from '../../navigation';
+import { BackButton } from '../../navigation';
+import { INITIAL_VALUES } from '../../../../modules/userProfile/userProfile.constants';
 
 describe('UserProfile: Component', () => {
   const component = props => <UserProfile {...defaultProps} {...props} />;
@@ -30,30 +32,42 @@ describe('UserProfile: Component', () => {
     global.expect(wrapper).toMatchSnapshot();
   });
 
-  it('should call updateMe on form submit', () => {
+  it('should call updateMe on Formik submit', () => {
     jest.spyOn(defaultProps, 'updateMe');
 
-    const wrapper = render();
+    const values = { sample: 'test' };
+    const wrapper = render({ isSettings: true });
 
-    wrapper
-      .find(Formik)
-      .dive()
-      .find(NextButton)
-      .simulate('click');
-    expect(defaultProps.updateMe).toBeCalled();
+    wrapper.find(Formik).prop('onSubmit')(values);
+    expect(defaultProps.updateMe).toBeCalledWith(values);
   });
 
-  it('should call makeAdmin on form submit', () => {
+  it('should call makeAdmin on Formik submit', () => {
     jest.spyOn(defaultProps, 'makeAdmin');
 
-    const wrapper = render();
+    const userId = 'id';
+    const match = { params: { userId } };
+    const wrapper = render({ match });
 
-    wrapper
-      .find(Formik)
-      .dive()
-      .find(NextButton)
-      .simulate('click');
-    expect(defaultProps.makeAdmin).toBeCalled();
+    wrapper.find(Formik).prop('onSubmit')();
+    expect(defaultProps.makeAdmin).toBeCalledWith({ userId });
+  });
+
+  it('should call handleSubmit on form submit', () => {
+    const renderProps = {
+      values: INITIAL_VALUES,
+      handleSubmit: Function.prototype,
+    };
+
+    jest.spyOn(renderProps, 'handleSubmit');
+
+    const instance = new UserProfile(defaultProps);
+    const wrapper = shallow(instance.renderContent(renderProps));
+    const sample = { sample: 'test' };
+
+    wrapper.find(Form).simulate('submit', sample);
+
+    expect(renderProps.handleSubmit).toBeCalledWith(sample);
   });
 
   it('should call handleChange on change of TextInput value', () => {
