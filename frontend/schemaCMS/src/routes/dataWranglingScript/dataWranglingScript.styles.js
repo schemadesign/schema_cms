c197bd1b49e49f5716d2aecbcbe995c468b1fafc
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-10-23 12:00:49 +0200

    fix styles

--- frontend/schemaCMS/src/routes/dataWranglingScript/dataWranglingScript.styles.js
+++ frontend/schemaCMS/src/routes/dataWranglingScript/dataWranglingScript.styles.js
@@ -1,8 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  margin: 0 20px;
-`;
+export const Container = styled.div``;
 
 export const Form = styled.form``;
 
