e99d4e396379b0fbb613e763c3062327c1704983
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-11-06 16:42:17 +0100

    update tests

--- frontend/schemaCMS/src/shared/components/contextHeader/__tests__/contextHeader.component.spec.js
+++ frontend/schemaCMS/src/shared/components/contextHeader/__tests__/contextHeader.component.spec.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import { ContextHeader } from '../contextHeader.component';
-import { defaultProps } from '../contextHeader.stories';
 
 describe('ContextHeader: Component', () => {
+  const defaultProps = {};
+
   const component = props => <ContextHeader {...defaultProps} {...props} />;
 
   const render = (props = {}) => shallow(component(props));
