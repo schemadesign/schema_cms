d227818848f00b8a91267c1ca5523971264caff9
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-11-08 15:21:11 +0100

    update storyshots

--- frontend/schemaCMS/src/routes/project/create/create.styles.js
+++ frontend/schemaCMS/src/routes/project/create/create.styles.js
@@ -1,12 +1,6 @@
 import styled from 'styled-components';
 
-import { media } from '../../../theme/media';
-
-export const Container = styled.div`
-  ${media.desktop`
-    margin-top: 70px;
-  `}
-`;
+export const Container = styled.div``;
 
 export const Form = styled.form`
   width: 100%;
