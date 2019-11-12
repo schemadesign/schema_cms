b8777e5d2dee789cc7c3c73e90be072c181c15f1
Krystian Hanek <khanek@apptension.com>
2019-11-12 15:17:45 +0100

    [CMS-455] Prevent hard delete project on soft delete action

--- backend/app/schemacms/projects/admin.py
+++ backend/app/schemacms/projects/admin.py
@@ -1,19 +1,24 @@
-import softdelete.admin
 from django.contrib import admin
 from django.db import transaction
 from django.utils import safestring
 from django.template.loader import render_to_string
 
 from . import models
+from schemacms.utils import admin as utils_admin
 
 
 admin.site.register(models.WranglingScript)
 
 
 @admin.register(models.Project)
-class Project(softdelete.admin.SoftDeleteObjectAdmin):
+class Project(utils_admin.SoftDeleteObjectAdmin):
     list_display = ("title", "owner", "status", "get_editors", "deleted_at")
     filter_horizontal = ("editors",)
+
+    def delete_selected(self, request, queryset):
+        return super().delete_queryset(request, queryset.exclude(deleted_at__isnull=0))
+
+    delete_selected.short_description = 'Soft delete selected objects'
 
     def get_queryset(self, request):
         return super().get_queryset(request).select_related("owner").prefetch_related("editors")
