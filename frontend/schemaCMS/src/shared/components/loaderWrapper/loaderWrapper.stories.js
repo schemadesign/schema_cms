3f3b17eaa3196401c8eb1fdf9f5b7fd69764d82a
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-11-14 10:41:04 +0100

    clean up

--- frontend/schemaCMS/src/shared/components/loaderWrapper/loaderWrapper.stories.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import { LoaderWrapper } from './loaderWrapper.component';
-
-const defaultProps = {};
-
-storiesOf('LoaderWrapper', module).add('Default', () => <LoaderWrapper {...defaultProps} />);
