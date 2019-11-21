d1114cb32a1d426d29c288039dfef947f4867bcc
Krystian Hanek <khanek@apptension.com>
2019-11-21 12:15:11 +0100

    [CMS-541] Add auth_method value to /me endpoint (#464)

--- backend/app/schemacms/users/serializers.py
+++ backend/app/schemacms/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+
 from .models import User
+from schemacms.authorization import constants as auth_constants
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,6 +9,28 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ("id", "username", "email", "first_name", "last_name", "role")
         read_only_fields = ("username", "role")
+
+
+class CurrentUserSerializer(serializers.ModelSerializer):
+    auth_method = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "email", "first_name", "last_name", "role", "auth_method")
+        read_only_fields = ("username", "role")
+
+    def get_auth_method(self, obj):
+        try:
+            token = self.context["request"].auth
+            if not token:
+                return None
+        except KeyError:
+            return None
+        try:
+            auth_method = auth_constants.JWTAuthMethod.get_method_from_jwt_token(token.decode())
+        except ValueError:
+            return None
+        return auth_method
 
 
 class UserSerializerForAdmin(serializers.ModelSerializer):
