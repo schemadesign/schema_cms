0b7c47c2c0bf352eb67e2e7c83080e026236746e
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-11-21 16:12:53 +0100

    style review fixes

--- frontend/schemaCMS/src/shared/components/errorContainer/errorContainer.styles.js
+++ frontend/schemaCMS/src/shared/components/errorContainer/errorContainer.styles.js
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
+import { always, cond, equals, T } from 'ramda';
 
 import { ERROR_TYPES } from './errorContainer.constants';
+
+const renderTypeSpecificStyles = cond([
+  [
+    equals(ERROR_TYPES.PAGE),
+    always(`
+    text-align: center;
+    padding: 50px 10px;
+    font-size: 20px;
+  `),
+  ],
+  [T, always('')],
+]);
 
 export const Container = styled.div`
   color: ${({ theme: { error } }) => error};
   font-weight: 200;
 
-  ${({ type }) =>
-    type === ERROR_TYPES.PAGE &&
-    `
-    text-align: center;
-    padding: 50px 10px;
-    font-size: 20px;
-  `}
+  ${({ type }) => renderTypeSpecificStyles(type)}
 `;
