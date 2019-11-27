d74d689474c0daf828021f9b2453a225f8357b5d
Szymon Jedrasiak <szymon@Szymons-MacBook-Pro.local>
2019-11-27 09:26:29 +0100

    [CMS-577] - pr fix #1

--- backend/app/schemacms/projects/receivers.py
+++ backend/app/schemacms/projects/receivers.py
@@ -1,7 +1,6 @@
 def update_meta_file(sender, instance, **kwargs):
-    instance_name = instance.__class__.__name__
 
-    if instance_name in ["Page", "Block", "Directory"]:
+    if hasattr(instance, "get_project"):
         project = instance.get_project()
         project.create_meta_file()
     else:
