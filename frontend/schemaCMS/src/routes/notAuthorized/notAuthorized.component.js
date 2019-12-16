ece351769772f3155109dcdb322b8b8175c310b0
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-12-16 11:17:10 +0100

    CMS-210 clean up not authorized & not found component

--- frontend/schemaCMS/src/routes/notAuthorized/notAuthorized.component.js
+++ frontend/schemaCMS/src/routes/notAuthorized/notAuthorized.component.js
@@ -1,20 +1,30 @@
-import React, { PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { Typography } from 'schemaUI';
 
 import messages from './notAuthorized.messages';
-import { Container } from './notAuthorized.styles';
+import { NoData } from '../../shared/components/noData';
 
 const { H1 } = Typography;
 
 export class NotAuthorized extends PureComponent {
+  static propTypes = {
+    intl: PropTypes.object.isRequired,
+  };
+
   render() {
     return (
-      <Container>
-        <H1>
-          <FormattedMessage {...messages.notAuthorized} />
-        </H1>
-      </Container>
+      <Fragment>
+        <Helmet title={this.props.intl.formatMessage(messages.pageTitle)} />
+
+        <NoData>
+          <H1>
+            <FormattedMessage {...messages.title} />
+          </H1>
+        </NoData>
+      </Fragment>
     );
   }
 }
--- frontend/schemaCMS/src/routes/notAuthorized/notAuthorized.stories.js
+++ frontend/schemaCMS/src/routes/notAuthorized/notAuthorized.stories.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { Theme } from 'schemaUI';
 
 import { NotAuthorized } from './notAuthorized.component';
+import { withTheme } from '../../.storybook/decorators';
+import { intl } from '../../.storybook/helpers';
 
-export const defaultProps = {};
+export const defaultProps = {
+  intl,
+};
 
-storiesOf('NotAuthorized', module).add('Default', () => <NotAuthorized {...defaultProps} />);
+storiesOf('NotAuthorized', module)
+  .addDecorator(withTheme(Theme.light))
+  .add('Default', () => <NotAuthorized {...defaultProps} />);
