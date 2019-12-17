d46196d6958c981e16c1119e30ab6b8f433f10ad
Marcin Pecelerowicz <mpecelerowicz@apptension.com>
2019-12-17 16:27:45 +0100

    CMS-692 adjust user list on invite page

--- frontend/schemaCMS/src/routes/project/addUser/addUser.styles.js
+++ frontend/schemaCMS/src/routes/project/addUser/addUser.styles.js
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
-import { Button as ButtonUI, Typography } from 'schemaUI';
-import { colors } from '../../../theme/styled';
+import { Button as ButtonUI } from 'schemaUI';
 
 import { media } from '../../../theme/media';
 
@@ -15,13 +14,13 @@ export const UserItem = styled.div`
   justify-content: space-between;
   margin: 5px 0;
   padding: 10px 0;
-  color: ${colors.grey};
-  border-top: 1px solid ${colors.grey};
+  color: ${({ theme }) => theme.secondaryText};
+  border-top: 2px solid ${({ theme }) => theme.border};
 `;
 
 export const UserItemDescription = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   flex-direction: column;
   flex: 1 1 auto;
 `;
@@ -30,59 +29,30 @@ export const Action = styled.div`
   display: flex;
   flex: 0 0 auto;
   margin-left: 10px;
-`;
-
-export const AddIcon = styled.div`
-  position: relative;
-  height: 15px;
-  width: 15px;
-  &:before {
-    content: '';
-    position: absolute;
-    height: 15px;
-    width: 2px;
-    background-color: ${colors.grey};
-    left: 50%;
-    transform: translateX(-50%);
-    top: 0;
-  }
-
-  &:after {
-    content: '';
-    position: absolute;
-    height: 2px;
-    width: 15px;
-    background-color: ${colors.grey};
-    top: 50%;
-    transform: translateY(-50%);
-    left: 0;
-  }
-
-  transform: rotate(90deg);
-`;
-
-export const RemoveIcon = styled.div`
-  position: relative;
-  height: 15px;
-  width: 15px;
-  &:after {
-    content: '';
-    position: absolute;
-    height: 2px;
-    width: 15px;
-    background-color: ${colors.grey};
-    top: 50%;
-    transform: translateY(-50%);
-    left: 0;
-  }
+  align-items: center;
 `;
 
 export const Button = styled(ButtonUI)`
   background-color: transparent !important;
 `;
 
-export const UserFullName = styled(Typography.H2)``;
-
-export const Email = styled(Typography.H3)`
-  word-break: break-all;
+export const UserFullName = styled.div`
+  font-size: 20px;
 `;
+
+export const Email = styled.div`
+  font-size: ${({ theme }) => theme.typography.span.fontSize};
+  word-break: break-all;
+  margin-top: 5px;
+`;
+
+export const iconStyles = {
+  width: 42,
+  height: 42,
+};
+
+export const buttonStyles = {
+  width: 42,
+  height: 42,
+  minHeight: 42,
+};
