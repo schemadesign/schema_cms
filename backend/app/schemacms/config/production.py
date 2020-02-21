01c3842458df814a04b71963b17c72cc75fc8fa5
Szymon <sjedrasiak@apptension.com>
2020-02-21 12:29:25 +0100

    s3file middleware removed

--- backend/app/schemacms/config/production.py
+++ backend/app/schemacms/config/production.py
@@ -7,10 +7,12 @@ class Production(Common):
     SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
     ALLOWED_HOSTS = ["*"]
-    INSTALLED_APPS += ("gunicorn", "storages", "s3file")
+    INSTALLED_APPS += (
+        "gunicorn",
+        "storages",
+    )
 
     MIDDLEWARE = Common.MIDDLEWARE
-    MIDDLEWARE += ("s3file.middleware.S3FileMiddleware",)
     AWS_HEADERS = {"Cache-Control": "max-age=86400, s-maxage=86400, must-revalidate"}
 
     SESSION_COOKIE_SECURE = True
