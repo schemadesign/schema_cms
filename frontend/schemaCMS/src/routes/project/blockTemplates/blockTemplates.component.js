d4b0830ee3fd776c2041de0ddb67bb4290185fb3
Rafał Gruszecki <rgruszecki@apptension.com>
2020-02-27 11:22:30 +0100

    Add react-use hooks

--- frontend/schemaCMS/src/routes/project/blockTemplates/blockTemplates.component.js
+++ frontend/schemaCMS/src/routes/project/blockTemplates/blockTemplates.component.js
@@ -1,18 +1,39 @@
-import React, { memo } from 'react';
+import React, { memo, useState } from 'react';
+import { useEffectOnce } from 'react-use';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
 import { Container } from './blockTemplates.styles';
 import messages from './blockTemplates.messages';
+import { LoadingWrapper } from '../../../shared/components/loadingWrapper';
 
-export const BlockTemplates = memo(() => {
+export const BlockTemplates = memo(({ fetchBlocks }) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffectOnce(() => {
+    (async () => {
+      try {
+        await fetchBlocks();
+      } catch (e) {
+        setError(e);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  });
+
   return (
     <Container>
       <FormattedMessage {...messages.title} />
+      <LoadingWrapper loading={loading} error={error}>
+        blocks
+      </LoadingWrapper>
     </Container>
   );
 });
 
 BlockTemplates.propTypes = {
   match: PropTypes.object.isRequired,
+  fetchBlocks: PropTypes.func.isRequired,
 };
