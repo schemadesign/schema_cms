31003f443d0a7b73f4ae244e98043d2640cb8018
Szymon <sjedrasiak@apptension.com>
2020-03-04 13:34:40 +0100

    CMS-960 top priority blocks elements added

--- backend/app/schemacms/pages/constants.py
+++ backend/app/schemacms/pages/constants.py
@@ -5,6 +5,17 @@ ELEMENT_NAME_MAX_LENGTH = 100
 class ElementType:
     RICH_TEXT = "rich_text"
     PLAIN_TEXT = "plain_text"
+    IMAGE = "image"
+    CODE = "code"
+    CONNECTION = "connection"
+    STACK = "stack"
 
 
-ELEMENT_TYPE_CHOICES = ((ElementType.RICH_TEXT, "Rich Text"), (ElementType.PLAIN_TEXT, "Plain Text"))
+ELEMENT_TYPE_CHOICES = (
+    (ElementType.RICH_TEXT, "Rich Text"),
+    (ElementType.PLAIN_TEXT, "Plain Text"),
+    (ElementType.IMAGE, "Image"),
+    (ElementType.CODE, "Code"),
+    (ElementType.CONNECTION, "Connection"),
+    (ElementType.STACK, "Stack"),
+)
