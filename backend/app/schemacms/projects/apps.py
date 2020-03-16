5bbeb0045782282da4ac109c710a75da653f3cb3
he4d <sjedrasiak@apptension.com>
2020-03-16 13:52:01 +0100

    public api objects updated on edit

--- backend/app/schemacms/projects/apps.py
+++ backend/app/schemacms/projects/apps.py
@@ -7,10 +7,17 @@ class ProjectsConfig(AppConfig):
 
     def ready(self):
         from ..utils import receivers
-        from . import models
+        from .models import Project
+        from ..datasources.models import DataSource
 
         db_signals.post_save.connect(
             receivers.update_public_api_meta,
-            sender=models.Project,
+            sender=Project,
+            dispatch_uid="utils.receivers.update_public_api_meta",
+        )
+
+        db_signals.post_save.connect(
+            receivers.update_public_api_meta,
+            sender=DataSource,
             dispatch_uid="utils.receivers.update_public_api_meta",
         )
