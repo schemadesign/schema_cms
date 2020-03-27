67dd645b37f030fe34dff2f8924a5ff66d4d119f
Szymon Jędrasiak <54309170+szdrasiak@users.noreply.github.com>
2020-03-27 15:06:47 +0100

    CMS-1034-035-1036 django admin fixes for pages and blocks
    * CMS-1034 adding new bloks to page template in django admin
    
    * CMS-1035-1036 django admin fixes

--- backend/app/schemacms/pages/admin.py
+++ backend/app/schemacms/pages/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 from django.db import transaction
-from softdelete.admin import SoftDeleteObjectInline
 
 from . import models
 from ..utils.admin import SoftDeleteObjectAdmin
 
 
-class ElementInline(admin.TabularInline):
+class CustomTabularInline(admin.TabularInline):
+    readonly_fields = ("deleted_at",)
+
+    def get_max_num(self, request, obj=None, **kwargs):
+        if obj and obj.deleted:
+            return 0
+        return self.min_num
+
+    def get_queryset(self, request):
+        qs = self.model._default_manager.all_with_deleted()
+        ordering = self.get_ordering(request) or ()
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
+
+    queryset = get_queryset
+
+
+class ElementInline(CustomTabularInline):
     model = models.BlockElement
-    exclude = ("deleted_at",)
     extra = 0
 
 
@@ -21,9 +37,8 @@ class BlockAdmin(SoftDeleteObjectAdmin):
     inlines = (ElementInline,)
 
 
-class BlockInline(SoftDeleteObjectInline):
+class BlockInline(CustomTabularInline):
     model = models.Page.blocks.through
-    exclude = ("deleted_at",)
     extra = 0
 
 
