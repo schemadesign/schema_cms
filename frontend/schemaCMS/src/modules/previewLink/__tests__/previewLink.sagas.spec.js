e98748e06254fda38e8fd8005d81f7fd22ed0c88
Daniel Capeletti <dcapeletti@icloud.com>
2020-04-20 10:23:01 +0200

    Update tests and snapshots

--- frontend/schemaCMS/src/modules/previewLink/__tests__/previewLink.sagas.spec.js
+++ frontend/schemaCMS/src/modules/previewLink/__tests__/previewLink.sagas.spec.js
@@ -8,14 +8,6 @@ import { fetchResponse } from '../previewLink.mocks';
 describe('PreviewLink: sagas', () => {
   const defaultState = Immutable({});
 
-  it('should implement a test', async () => {
-    await expectSaga(watchPreviewLink)
-      .withState(defaultState)
-      .put(PreviewLinkRoutines.noop.success())
-      .dispatch(PreviewLinkRoutines.noop())
-      .silentRun();
-  });
-
   describe('when /FETCH_LINK action is fired', () => {
     it('should put fetchLink action', async () => {
       const url = 'https://thisisanurl.com';
