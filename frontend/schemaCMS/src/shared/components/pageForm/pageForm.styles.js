1e6f92e6165bdc3e6cbefb7271159c0196d2066d
Rafał Gruszecki <rgruszecki@apptension.com>
2020-05-07 10:10:26 +0200

    CMS-1125 Add space on bottom page form

--- frontend/schemaCMS/src/shared/components/pageForm/pageForm.styles.js
+++ frontend/schemaCMS/src/shared/components/pageForm/pageForm.styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding-bottom: 50px;
+`;
+
 export const SelectContainer = styled.div`
   position: relative;
   z-index: 999;
