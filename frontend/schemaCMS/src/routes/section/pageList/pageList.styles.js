577574e05b862d2338300b7ef9bcd80bd6156bb2
Rafał Gruszecki <rgruszecki@apptension.com>
2020-06-08 10:40:51 +0200

    CMS-1278 ellipsis copy in card footer

--- frontend/schemaCMS/src/routes/section/pageList/pageList.styles.js
+++ frontend/schemaCMS/src/routes/section/pageList/pageList.styles.js
@@ -4,6 +4,13 @@ export const Container = styled.div``;
 
 export const Form = styled.form``;
 
+export const CardFooter = styled.div`
+  width: 100%;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
 export const getCustomHomeIconStyles = ({ active }) => ({
   opacity: active ? 1 : 0.3,
   cursor: 'pointer',
