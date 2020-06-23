eb0fb532c2ffc83f700c0bea35288aff3ce5a4af
Szymon Jędrasiak <54309170+szdrasiak@users.noreply.github.com>
2020-06-23 13:50:27 +0200

    CMS-1333 state tages in public api fix (#993)

--- backend/app/schemacms/states/tests/factories.py
+++ backend/app/schemacms/states/tests/factories.py
@@ -2,6 +2,7 @@ import factory
 
 from schemacms.datasources.tests.factories import DataSourceFactory
 from schemacms.users.tests.factories import UserFactory
+from schemacms.tags.tests.factories import TagCategoryFactory
 
 
 class StateFactory(factory.django.DjangoModelFactory):
@@ -12,3 +13,12 @@ class StateFactory(factory.django.DjangoModelFactory):
     name = factory.Faker("text", max_nb_chars=50)
     description = factory.Faker("text", max_nb_chars=150)
     author = factory.SubFactory(UserFactory)
+
+
+class StateTagFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = "states.StateTag"
+
+    state = factory.SubFactory(StateFactory)
+    category = factory.SubFactory(TagCategoryFactory)
+    value = factory.Faker("text", max_nb_chars=150)
