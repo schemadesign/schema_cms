9e1d4415377565872acd2386a4fec622d690659e
Tomasz Agacinski <tagacinski@apptension.com>
2020-07-23 16:00:04 +0200

    remove unused code

--- frontend/schemaPreviewExtension/src/js/background.js
+++ frontend/schemaPreviewExtension/src/js/background.js
@@ -1,8 +1,3 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
 
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.executeScript({
-        file: 'contentScript.js'
-    });
-});
