0d205945852d9b29fa3d5130e869935a834e2992
Tomasz Agacinski <tagacinski@apptension.com>
2020-07-23 15:21:38 +0200

    remove unused code

--- frontend/schemaPreviewExtension/src/js/popup/schemaPreview.js
+++ frontend/schemaPreviewExtension/src/js/popup/schemaPreview.js
@@ -1,21 +1,18 @@
 export default function () {
 
   let toggleSwitch = document.getElementById('switch-button');
-  let x  = document.getElementById('myid')
   let toggleSwitchValue = false;
 
   toggleSwitch.addEventListener('change', ()=> {
     if (!toggleSwitchValue) {
       toggleSwitchValue = true;
       toggleSwitch.checked = toggleSwitchValue;
-      x.classList.add('test');
       return chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: true, dataId: 2 });
       });
     }
     toggleSwitchValue = false;
     toggleSwitch.checked = toggleSwitchValue;
-    x.classList.remove('test');
     return chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: false, dataId: 1 });
     });
