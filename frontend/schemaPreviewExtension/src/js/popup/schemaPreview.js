18729783d74a5679097f9b7ef227c8648da8cc60
Tomasz Agacinski <tagacinski@apptension.com>
2020-07-24 14:45:46 +0200

    add local storage to keep data persist

--- frontend/schemaPreviewExtension/src/js/popup/schemaPreview.js
+++ frontend/schemaPreviewExtension/src/js/popup/schemaPreview.js
@@ -1,20 +1,34 @@
 export default function () {
 
   let toggleSwitch = document.getElementById('switch-button');
-  let toggleSwitchValue = false;
+  let toggleSwitchValue = null;
+
+  window.onload = function() {
+    chrome.storage.local.get("isPreviewMode", function(value){
+      toggleSwitchValue = value.isPreviewMode;
+      toggleSwitch.checked = toggleSwitchValue;
+    });
+    return chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: toggleSwitchValue, dataId: 0 });
+    });
+  }
 
   toggleSwitch.addEventListener('change', ()=> {
     if (!toggleSwitchValue) {
       toggleSwitchValue = true;
       toggleSwitch.checked = toggleSwitchValue;
+      chrome.storage.local.set({ "isPreviewMode": toggleSwitchValue }, function(){
+      });
       return chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: true, dataId: 2 });
+        chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: toggleSwitchValue, dataId: 2 });
       });
     }
     toggleSwitchValue = false;
     toggleSwitch.checked = toggleSwitchValue;
+    chrome.storage.local.set({ "isPreviewMode": toggleSwitchValue }, function(){
+    });
     return chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: false, dataId: 1 });
+      chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: toggleSwitchValue, dataId: 1 });
     });
   })
 
