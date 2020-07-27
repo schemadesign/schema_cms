eec004520bcc6d5be9f0212ed3a3778454b15a87
Tomasz Agacinski <tagacinski@apptension.com>
2020-07-27 15:03:12 +0200

    reset state to default after web page reloading

--- frontend/schemaPreviewExtension/src/contentScript.js
+++ frontend/schemaPreviewExtension/src/contentScript.js
@@ -1,4 +1,10 @@
-
 chrome.runtime.onMessage.addListener((request, sender) => {
     return window.postMessage({ type: "SCHEMA_CMS", PREVIEW_MODE: request.isPreviewMode, DATA_ID: request.dataId }, "*");
 });
+
+window.addEventListener('message', (event) => {
+    if (event.data.type && (event.data.type == "FROM_PAGE")) {
+        chrome.storage.local.set({ "isPreviewMode": false });
+    }
+})
+
