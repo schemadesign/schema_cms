db2a3ea04f0669ed4ab893e628fb88d7acea0091
Tomasz Agacinski <tagacinski@apptension.com>
2020-07-27 12:21:19 +0200

    fix change content

--- frontend/schemaPreviewExtension/src/js/popup/schemaPreview.js
+++ frontend/schemaPreviewExtension/src/js/popup/schemaPreview.js
@@ -4,7 +4,6 @@ export default () => {
   let toggleSwitchValue = null;
 
   window.onload = () => {
-    console.log('popup', chrome.storage.local, chrome.tabs)
     chrome.storage.local.get("isPreviewMode", (value) => {
       toggleSwitchValue = value.isPreviewMode;
       toggleSwitch.checked = toggleSwitchValue;
@@ -18,19 +17,16 @@ export default () => {
     if (!toggleSwitchValue) {
       toggleSwitchValue = true;
       toggleSwitch.checked = toggleSwitchValue;
-      chrome.storage.local.set({ "isPreviewMode": toggleSwitchValue }, () => {
-      });
+      chrome.storage.local.set({ "isPreviewMode": toggleSwitchValue });
       return chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
         chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: toggleSwitchValue, dataId: 2 });
       });
     }
     toggleSwitchValue = false;
     toggleSwitch.checked = toggleSwitchValue;
-    chrome.storage.local.set({ "isPreviewMode": toggleSwitchValue }, () => {
-    });
+    chrome.storage.local.set({ "isPreviewMode": toggleSwitchValue });
     return chrome.tabs.query({ active: true, currentWindow: true }, (tabs)  =>{
       chrome.tabs.sendMessage(tabs[0].id, { isPreviewMode: toggleSwitchValue, dataId: 1 });
     });
   })
-
 };
