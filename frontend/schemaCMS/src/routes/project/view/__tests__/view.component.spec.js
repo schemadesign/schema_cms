4efe03ff25affee321a8008d40d61fc8c4bac0c2
Daniel Capeletti <dcapeletti@apptension.com>
2020-08-03 14:07:47 +0200

    Update IDs and add more tests for the publish modal

--- frontend/schemaCMS/src/routes/project/view/__tests__/view.component.spec.js
+++ frontend/schemaCMS/src/routes/project/view/__tests__/view.component.spec.js
@@ -5,6 +5,7 @@ import { View } from '../view.component';
 import { defaultProps } from '../view.stories';
 import { NextButton } from '../../../../shared/components/navigation';
 import { TextInput } from '../../../../shared/components/form/inputs/textInput';
+import { PROJECT_STATUSES } from '../../../../modules/project/project.constants';
 
 describe('View: Component', () => {
   const component = props => <View {...defaultProps} {...props} />;
@@ -48,6 +49,47 @@ describe('View: Component', () => {
       .simulate('click');
 
     expect(defaultProps.removeProject).toHaveBeenCalledWith({ projectId: '100' });
+  });
+
+  it('should call setFieldValue on select published project status', async () => {
+    defaultProps.fetchProject = jest.fn().mockReturnValue(Promise.resolve());
+    jest.spyOn(defaultProps, 'setFieldValue');
+
+    const wrapper = await render();
+
+    wrapper
+      .find('#projectStatusSelect')
+      .props()
+      .onSelect({ value: PROJECT_STATUSES.PUBLISHED });
+
+    expect(wrapper.state().publishConfirmationModalOpen).toBeTruthy();
+
+    wrapper
+      .find('#projectPublishConfirmationModalConfirmBtn')
+      .last()
+      .simulate('click');
+
+    expect(defaultProps.setFieldValue).toHaveBeenCalledWith('status', 'published');
+  });
+
+  it('should set publishConfirmationModalOpen to false', async () => {
+    defaultProps.fetchProject = jest.fn().mockReturnValue(Promise.resolve());
+
+    const wrapper = await render();
+
+    wrapper
+      .find('#projectStatusSelect')
+      .props()
+      .onSelect({ value: PROJECT_STATUSES.PUBLISHED });
+
+    expect(wrapper.state().publishConfirmationModalOpen).toBeTruthy();
+
+    wrapper
+      .find('#projectConfirmationPublishModalCancelBtn')
+      .last()
+      .simulate('click');
+
+    expect(wrapper.state().publishConfirmationModalOpen).toBeFalsy();
   });
 
   it('should call handleSubmit on save', async () => {
