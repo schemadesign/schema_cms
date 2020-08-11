c9f756c87a9ae11d6c7c9f77197015ec918b7322
Szymon Jędrasiak <54309170+szdrasiak@users.noreply.github.com>
2020-08-11 11:28:05 +0200

    CMS-1500 copy page creates only draft instance, CMS-1499 invitation email subject
    * CMS-1500 copy page creates only draft instance, CMS-1499 new invitation email subject
    
    * package.json version updated
    
    * package.json removed

--- backend/app/schemacms/pages/views.py
+++ backend/app/schemacms/pages/views.py
@@ -299,6 +299,12 @@ class PageViewSet(DetailViewSet):
 
         try:
             new_page = page.copy_page()
+
+            published_version = new_page.copy_page(attrs={"is_draft": False})
+
+            new_page.published_version = published_version
+            new_page.save()
+
         except Exception as e:
             return response.Response({"error": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
 
