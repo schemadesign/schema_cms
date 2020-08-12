f2db6854bb95698a04fe06b54d7d00bb5f0f625e
arkbuchholz <buchholzarkadiusz@gmail.com>
2020-08-12 15:05:20 +0200

    Update configuration

---
+++ frontend/E2ETests/config/firefox.conf.js
@@ -0,0 +1,7 @@
+export default {
+  maxInstances: 1,
+  browserName: 'firefox',
+  'moz:firefoxOptions': {
+    args: process.env.HEADLESS ? ['--headless'] : [''],
+  },
+};
---
+++ frontend/E2ETests/config/safari.conf.js
@@ -0,0 +1,4 @@
+export default {
+  maxInstances: 1,
+  browserName: 'safari',
+};
