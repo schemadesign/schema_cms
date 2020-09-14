f29cf3a77f12d08dcb7cf1ee13e78d73a6a7ee5f
Daniel Capeletti <dcapeletti@apptension.com>
2020-09-14 12:41:08 +0200

    Add tests

--- frontend/schemaCMS/src/modules/config/__tests__/config.sagas.spec.js
+++ frontend/schemaCMS/src/modules/config/__tests__/config.sagas.spec.js
@@ -1,17 +1,27 @@
 import { expectSaga } from 'redux-saga-test-plan';
 import Immutable from 'seamless-immutable';
+import { OK } from 'http-status-codes';
 
 import { watchConfig } from '../config.sagas';
 import { ConfigRoutines } from '../config.redux';
+import mockApi from '../../../shared/utils/mockApi';
+import { OKTA_BACKEND } from '../config.constants';
 
 describe('Config: sagas', () => {
   const defaultState = Immutable({});
 
-  it('should implement a test', async () => {
+  it('should fetch config', async () => {
+    const response = {
+      authenticationBackend: OKTA_BACKEND,
+    };
+
+    mockApi.get('/config').reply(OK, response);
+
     await expectSaga(watchConfig)
       .withState(defaultState)
-      .put(ConfigRoutines.noop.success())
-      .dispatch(ConfigRoutines.noop())
+      .put(ConfigRoutines.fetchConfig.request())
+      .put(ConfigRoutines.fetchConfig.success(response))
+      .dispatch(ConfigRoutines.fetchConfig())
       .silentRun();
   });
 });
--- frontend/schemaCMS/src/modules/config/__tests__/config.selectors.spec.js
+++ frontend/schemaCMS/src/modules/config/__tests__/config.selectors.spec.js
@@ -1,16 +1,51 @@
 import { expect } from 'chai';
 import Immutable from 'seamless-immutable';
 
-import { selectConfigDomain } from '../config.selectors';
+import { selectConfigDomain, selectBackend, selectIsOktaBackend, selectIsAuth0Backend } from '../config.selectors';
+import { AUTH0_BACKEND, OKTA_BACKEND } from '../config.constants';
 
 describe('Config: selectors', () => {
   const state = Immutable({
-    config: {},
+    config: {
+      authenticationBackend: null,
+    },
   });
 
   describe('selectConfigDomain', () => {
     it('should select a domain', () => {
       expect(selectConfigDomain(state)).to.equal(state.config);
+    });
+  });
+
+  describe('selectBackend', () => {
+    it('should select the backend', () => {
+      expect(selectBackend(state)).to.equal(state.config.authenticationBackend);
+    });
+  });
+
+  describe('selectIsOktaBackend', () => {
+    it('should select a if is okta backend', () => {
+      const oktaState = Immutable({
+        config: {
+          authenticationBackend: OKTA_BACKEND,
+        },
+      });
+
+      expect(selectIsOktaBackend(oktaState)).to.equal(true);
+      expect(selectIsOktaBackend(state)).to.equal(false);
+    });
+  });
+
+  describe('selectIsAuth0Backend', () => {
+    it('should select a if is auth backend', () => {
+      const auth0State = Immutable({
+        config: {
+          authenticationBackend: AUTH0_BACKEND,
+        },
+      });
+
+      expect(selectIsAuth0Backend(auth0State)).to.equal(true);
+      expect(selectIsAuth0Backend(state)).to.equal(false);
     });
   });
 });
