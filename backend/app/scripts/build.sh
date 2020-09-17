6bfc20e5f08543097af11d86d94e083029dafeec
Szymon Jedrasiak <sjedrasiak@apptension.com>
2020-09-17 09:33:01 +0200

    handle aws cli verions 1 and 2 on pushing images to ECR

--- backend/app/scripts/build.sh
+++ backend/app/scripts/build.sh
@@ -10,8 +10,13 @@ TASK_IMAGE_URI_BACKEND=$BACKEND_REPO_URI:$VERSION
 docker build -t "$TASK_IMAGE_URI_BACKEND" .
 docker tag "$TASK_IMAGE_URI_BACKEND" "$BACKEND_REPO_URI:latest"
 
-# shellcheck disable=SC2091
-$(aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
+if DOCKER_LOGIN=$(aws ecr get-login-password --region "$AWS_DEFAULT_REGION" 2> /dev/null); then
+  echo "$DOCKER_LOGIN" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com
+else
+  echo "get-login-password not supported by the AWS CLI, trying get-login instead..."
+  # shellcheck disable=SC2091
+  $(aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
+fi
 
 docker push "$TASK_IMAGE_URI_BACKEND"
 docker push "$BACKEND_REPO_URI:latest"
