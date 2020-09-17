9c369ab13833122f5324aa90f847386f1a4760be
Szymon Jędrasiak <54309170+szdrasiak@users.noreply.github.com>
2020-09-17 10:21:04 +0200

    handle aws cli verions 1 and 2 on pushing images to ECR (#1087)

--- nginx/build.sh
+++ nginx/build.sh
@@ -13,8 +13,13 @@ docker build -t "$TASK_IMAGE_URI_NGINX" .
 docker tag "$TASK_IMAGE_URI_NGINX" "$NGINX_REPO_URI:latest"
 
 
-# shellcheck disable=SC2091
-$(aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
+if DOCKER_LOGIN=$(aws ecr get-login-password --region "$AWS_DEFAULT_REGION" 2> /dev/null); then
+  echo "$DOCKER_LOGIN" | docker login --username AWS --password-stdin "$AWS_ACCOUNT_ID".dkr.ecr."$AWS_DEFAULT_REGION".amazonaws.com
+else
+  echo "get-login-password not supported by the AWS CLI, trying get-login instead..."
+  # shellcheck disable=SC2091
+  $(aws ecr get-login --no-include-email --region "$AWS_DEFAULT_REGION")
+fi
 
 docker push "$TASK_IMAGE_URI_NGINX"
 docker push "$NGINX_REPO_URI:latest"
