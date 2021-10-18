db04fae347a7103e037388a90eb49a6f45c481d6
Paweł Mikutel <pmikutel@apptension.com>
2021-10-18 16:07:36 +0200

    Refactor local test update script to remove cache, get rid of docker-compose and mini opimalization for docker file

--- frontend/schemaCMS/scripts/run_test_outside.sh
+++ frontend/schemaCMS/scripts/run_test_outside.sh
@@ -5,28 +5,15 @@ set -Eexo pipefail
 CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 cd $CWD/../..
 
-export APP="SchemaCMS"
+export APP="schema-cms"
 export TEST_ENV="jest"
 export PROJECT_NAME="$APP${CIRCLE_SHA1-}$TEST_ENV"
 VOLUME_NAME="_snapshot_output"
 export COMPOSE_DOCKER_CLI_BUILD=1
 
-function cleanup() {
-  docker-compose --project-name $PROJECT_NAME down
-  docker network rm ${CIRCLE_SHA1-}${APP}${TEST_ENV}js || echo "no network found to remove"
-}
-
-trap cleanup EXIT
-
-
-docker volume rm "$PROJECT_NAME$VOLUME_NAME" || echo "No volume to delete"
-docker-compose --project-name $PROJECT_NAME -f ./docker-compose.yml build test
+docker build --tag $PROJECT_NAME --file ./DockerfileBase .
 
 # Run the tests, and copy the new snapshots out
 echo "Updating Snapshots"
-docker-compose --project-name $PROJECT_NAME run test npm run test -- -u
-CONTAINER_ID=$(docker-compose --project-name $PROJECT_NAME run -d test sleep 10000)
-docker cp $CONTAINER_ID:/webapp/schemaCMS/src ./schemaCMS
-docker kill $CONTAINER_ID
-
-cleanup
+# TODO investigate how we can automatically close Jest ( --watchAll=false --forceExit flags doesnt work)
+docker run --volume $(pwd)/schemaCMS/src:/webapp/schemaCMS/src --rm --interactive --tty $PROJECT_NAME npm run test -- -u
